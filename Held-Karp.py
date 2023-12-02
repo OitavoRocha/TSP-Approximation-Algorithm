2f57860180018983e924ca090f031f793d1cf36a
Otávio Rocha <otaviosrocha30@gmail.com>
2023-12-02 20:11:49 -0300

    Update Held-Karp.py

--- Held-Karp.py
+++ Held-Karp.py
@@ -1,8 +1,9 @@
 # https://github.com/CarlEkerot/held-karp/tree/master
 import itertools
+import sys
 
 
-def read_adjacency_matrix(file_path):
+def readAdjacencyMatrix(file_path):
     try:
         with open(file_path, 'r') as file:
             lines = file.readlines()
@@ -18,38 +19,20 @@ def read_adjacency_matrix(file_path):
         return None
 
 
-def held_karp(dists):
-    """
-    Implementation of Held-Karp, an algorithm that solves the Traveling
-    Salesman Problem using dynamic programming with memoization.
-
-    Parameters:
-        dists: distance matrix
-
-    Returns:
-        A tuple, (cost, path).
-    """
+def heldKarp(dists):
     n = len(dists)
-
-    # Maps each subset of the nodes to the cost to reach that subset, as well
-    # as what node it passed before reaching this subset.
-    # Node subsets are represented as set bits.
     C = {}
 
-    # Set transition cost from initial state
     for k in range(1, n):
         C[(1 << k, k)] = (dists[0][k], 0)
-
-    # Iterate subsets of increasing length and store intermediate results
-    # in classic dynamic programming manner
+    
     for subset_size in range(2, n):
         for subset in itertools.combinations(range(1, n), subset_size):
-            # Set bits for all nodes in this subset
+            
             bits = 0
             for bit in subset:
                 bits |= 1 << bit
 
-            # Find the lowest cost to get to this subset
             for k in subset:
                 prev = bits & ~(1 << k)
 
@@ -60,16 +43,14 @@ def held_karp(dists):
                     res.append((C[(prev, m)][0] + dists[m][k], m))
                 C[(bits, k)] = min(res)
 
-    # We're interested in all bits but the least significant (the start state)
+    
     bits = (2**n - 1) - 1
 
-    # Calculate optimal cost
     res = []
     for k in range(1, n):
         res.append((C[(bits, k)][0] + dists[k][0], k))
     opt, parent = min(res)
 
-    # Backtrack to find full path
     path = []
     for i in range(n - 1):
         path.append(parent)
@@ -77,18 +58,17 @@ def held_karp(dists):
         _, parent = C[(bits, parent)]
         bits = new_bits
 
-    # Add implicit start state
     path.append(0)
 
     return opt, list(reversed(path))
 
 
 def main():
-    arg = "tsp4_7013.txt"
+    arg =  sys.argv[1]
 
-    dists = read_adjacency_matrix(arg)
+    dists = readAdjacencyMatrix(arg)
 
-    print(held_karp(dists))
+    print(heldKarp(dists))
 
 
 if __name__ == '__main__':
