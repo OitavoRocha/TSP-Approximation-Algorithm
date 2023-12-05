f6aebc5436358b7ef9258366d35b2d1f1605cab2
João Vitor Farias <102336880+joaovfarias@users.noreply.github.com>
2023-12-05 18:47:44 -0300

    Update Christofides.py

--- Christofides.py
+++ Christofides.py
@@ -1,8 +1,37 @@
 import numpy as np
 import time
 import sys
+import networkx as nx
 
 INT_MAX = 9999999
+
+def main():
+    num_edges = 1
+    args = sys.argv[1]
+
+    matrix_values = readAdjacencyMatrix(args)
+    nodes = len(matrix_values)
+    matrix = np.array(matrix_values)
+
+    MinSpanTree, cost = prim(matrix, nodes, num_edges)
+    minSpanTreeEdges = findAllEdges(MinSpanTree)
+
+    oddDegreeNodes = findOddDegreeNodes(MinSpanTree)
+
+    minPerfectWeightMatchingEdges = minimumPerfectWeightMatching(matrix, oddDegreeNodes)
+    minPerfectWeightMatchingEdges.extend(minSpanTreeEdges)
+
+    fleury(minPerfectWeightMatchingEdges, matrix)
+    
+
+def fleury(edges, matrix):
+    G = nx.MultiGraph()
+    for edge in edges:
+        G.add_edge(edge[0], edge[1])
+
+    circuit = list(nx.eulerian_circuit(G, source=0))
+    cost = calculateCost(circuit, matrix)
+    print("Cost: ", cost)
 
 def prim(matrix, nodes, num_edges):
     if num_edges == 0:
@@ -15,15 +44,12 @@ def prim(matrix, nodes, num_edges):
 
     for i in range(nodes):
         parents[i] = 0
-
     parents[0] = 1
 
     i = 0
     while i < nodes - 1:
         min_key = INT_MAX
-
         x, y = 0, 0
-
         for j in range(nodes):
             if parents[j] == 1:
                 for k in range(nodes):
@@ -31,7 +57,6 @@ def prim(matrix, nodes, num_edges):
                         if min_key > matrix[j][k]:
                             min_key = matrix[j][k]
                             x, y = j, k
-
         edge = (x, y)
         minimum_spanning_tree.append(edge)
         cost += matrix[x][y]
@@ -47,72 +72,40 @@ def prim(matrix, nodes, num_edges):
     adjacency_matrix = np.array(adjacency_matrix)
     return adjacency_matrix, cost
 
-def main():
-    num_edges = 1
-    args = sys.argv[1]
-
-    matrix_values = readAdjacencyMatrix(args)
-    nodes = len(matrix_values)
-    
-    matrix = np.array(matrix_values)
-
-    MinSpanTree, cost = prim(matrix, nodes, num_edges)
-    minimumPerfectWeightMatching(MinSpanTree, matrix, findOddDegreeNodes(MinSpanTree))
-
-    cycle = fleuryAlgo(MinSpanTree, 0)
-    cycle = removeDuplicates(cycle)
-    print(calculateCost(cycle, matrix))
-
-
 def findOddDegreeNodes(minSpanTree):
     oddDegreeNodes = []
-    for node in range(len(minSpanTree)):
-        degree = 0
-        for edge in range(len(minSpanTree)):
-            if minSpanTree[node][edge] != 0:
-                degree += 1
-        if degree % 2 != 0:
-            oddDegreeNodes.append(node)
+    for i in range(len(minSpanTree)):
+        deegre = 0
+        for j in range(len(minSpanTree)):
+            if minSpanTree[i][j] != 0:
+                deegre += 1
+        if deegre % 2 == 1:
+            oddDegreeNodes.append(i)
     return oddDegreeNodes
 
-def minimumPerfectWeightMatching(MinSpanTree, G, odd_vert):
+def minimumPerfectWeightMatching(G, odd_vert):
+    edges = []
     used = []
     all_matches = [(G[u][v], u, v) for u in odd_vert for v in odd_vert if u != v and u < v]
     all_matches.sort()
     for i in range(len(all_matches)):
         if all_matches[i][1] not in used and all_matches[i][2] not in used:
-            MinSpanTree[all_matches[i][1]][all_matches[i][2]] = all_matches[i][0]  
-            MinSpanTree[all_matches[i][2]][all_matches[i][1]] = all_matches[i][0]
+            edges.append((all_matches[i][1], all_matches[i][2]))
             used.append(all_matches[i][1])
             used.append(all_matches[i][2])
         if len(used) == len(odd_vert):
             break
-    
-def fleuryAlgo(MinSpanTree, startNode):
-    stack = []
-    stack.append(startNode)
-    circuit = []
-    while len(stack) != 0:
-        top = stack[-1]
-        for i in range(len(MinSpanTree)):
-            if MinSpanTree[top][i] != 0:
-                stack.append(i)
-                MinSpanTree[top][i] = 0
-                MinSpanTree[i][top] = 0
-                break
-        if top == stack[-1]:
-            circuit.append(stack.pop())
-    return circuit
+    return edges
 
-def removeDuplicates(circuit):
-    cycle = [circuit[i] for i in range(len(circuit)) if circuit[i] not in circuit[:i]]
-    cycle.append(circuit[0])
-    return cycle
 
-def calculateCost(cycle, G):
+def findAllEdges(graph):
+    edges = [(u, v) for u in range(len(graph)) for v in range(len(graph)) if graph[u][v] != 0 and u < v]
+    return edges
+
+def calculateCost(circuit, G):
     cost = 0
-    for i in range(len(cycle)-1):
-        cost += G[cycle[i]][cycle[i+1]]
+    for edge in circuit:
+        cost += G[edge[0]][edge[1]]
     return cost
 
 def readAdjacencyMatrix(file_path):
